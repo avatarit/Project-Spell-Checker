a1d5f8d01cc5a214aa2537e15beb4474a938a778
avatarit <salah@ait.ps>
2026-02-24 05:30:02 +0000

    Refactor HTML structure and update script to display dictionary size

--- script.mjs
+++ script.mjs
@@ -4,8 +4,11 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import getDictionarySize from "./common.mjs";
+import {getDictionarySize} from "./common.mjs";
+const dictionarySize = getDictionarySize();
+const wordSize= document.getElementById("dictSize");
 
 window.onload = function() {
-    document.querySelector("body").innerText = `There are ${getDictionarySize()} words in the Basic English dictionary`;
+
+  wordSize.textContent = `${dictionarySize}`;
 }
